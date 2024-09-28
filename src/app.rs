c9297e428bf123b9e93b1fb16e61f001aacd779a
fourarmy <57693631+m1ten@users.noreply.github.com>
2024-09-28 19:17:16 -0400

    move to seperate app file

---
+++ src/app.rs
@@ -0,0 +1,101 @@
+use ratatui::crossterm::terminal;
+use ratatui::layout::{Alignment, Constraint, Direction, Layout};
+use ratatui::style::{Color, Style};
+use ratatui::text::ToText;
+use ratatui::widgets::{Block, Borders, Paragraph};
+use ratatui::Frame;
+
+enum Scene {
+    Main,
+    Play,
+    Options,
+}
+
+impl Scene {
+    fn fmt(&self) -> String {
+        match self {
+            Scene::Main => "Main".to_string(),
+            Scene::Play => "Play".to_string(),
+            Scene::Options => "Options".to_string(),
+        }
+    }
+}
+
+pub(crate) struct App {
+    scene: Scene,
+    console_style: Style,
+}
+
+impl App {
+    pub fn new() -> App {
+        App {
+            scene: Scene::Main,
+            console_style: Style::default(),
+        }
+    }
+
+    pub fn draw(&self, f: &mut Frame, title: &str, input: &str) {
+        if f.area().height * f.area().width < 2400 {
+            f.render_widget(
+                Paragraph::new("Please resize the terminal to at least 80x30.")
+                    .block(Block::default().borders(Borders::ALL).title("Error"))
+                    .alignment(Alignment::Center),
+                f.area(),
+            );
+            return;
+        }
+
+        let title_wg = Paragraph::new(ratatui::style::Stylize::bold(title))
+            .block(Block::default().borders(Borders::ALL).title("Title"))
+            .alignment(Alignment::Center);
+
+        let screen_wg = Paragraph::new(self.scene.fmt())
+            .block(Block::default().borders(Borders::ALL).title("Scene"))
+            .alignment(Alignment::Center);
+
+        let console_wg = Paragraph::new(format!("🔥 {}", input))
+            .block(Block::default().borders(Borders::ALL).title("Console"))
+            .style(self.console_style)
+            .alignment(Alignment::Left);
+
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            // .margin(2)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10),
+                ]
+                .as_ref(),
+            )
+            .split(f.area());
+
+        f.render_widget(title_wg, chunks[0]);
+        f.render_widget(screen_wg, chunks[1]);
+        f.render_widget(console_wg, chunks[2]);
+    }
+
+    pub fn handle_input(&mut self, input: &str) -> bool {
+        match input {
+            "quit" | "exit" => return false,
+            "play" => {
+                self.console_style = Style::default();
+                self.scene = Scene::Play;
+            }
+            "options" | "settings" => {
+                self.console_style = Style::default();
+                self.scene = Scene::Options;
+            }
+            "main" | "home" => {
+                self.console_style = Style::default();
+                self.scene = Scene::Main;
+            }
+            _ => {
+                self.console_style = Style::default().fg(Color::Red);
+            }
+        }
+
+        true
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,195 +1,48 @@
-use std::io;
+mod App;
 
-use ratatui::{
-    backend::CrosstermBackend,
-    crossterm::{
-        self,
-        event::{self, KeyCode, KeyEventKind},
-        terminal::{disable_raw_mode, enable_raw_mode},
-        ExecutableCommand,
-    },
-    layout::{Alignment, Constraint, Direction, Layout},
-    style::Style,
-    widgets::{Block, Borders, Paragraph},
-    Terminal,
-};
+use crossterm::event::{self, Event, KeyCode};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use ratatui::backend::CrosstermBackend;
+use ratatui::{crossterm, Terminal};
+use std::io::{self};
 
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    verbose: bool,
-}
-
-fn main() -> io::Result<()> {
-    let _args = Args::parse();
-
+fn main() -> Result<(), io::Error> {
     enable_raw_mode()?;
     let mut stdout = io::stdout();
-    stdout.execute(crossterm::terminal::EnterAlternateScreen)?;
+    execute!(stdout, crossterm::terminal::EnterAlternateScreen)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let mut app = App {
-        title: format!("asciiLife"),
-        ..Default::default()
-    };
-    let app_result = app.run(&mut terminal);
+    let mut input = String::new();
+    let mut app = App::App::new();
+
+    loop {
+        terminal.draw(|f| app.draw(f, "asciiLife", &input))?;
+
+        if event::poll(std::time::Duration::from_millis(200))? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char(c) => input.push(c),
+                    KeyCode::Backspace => {
+                        input.pop();
+                    }
+                    KeyCode::Enter => match app.handle_input(&input) {
+                        true => (),
+                        false => break,
+                    },
+                    KeyCode::Esc => break,
+                    _ => {}
+                }
+            }
+        }
+    }
 
     disable_raw_mode()?;
-    crossterm::execute!(
+    execute!(
         terminal.backend_mut(),
         crossterm::terminal::LeaveAlternateScreen
     )?;
     terminal.show_cursor()?;
-
-    app_result
-}
-
-#[derive(Debug)]
-enum Scene {
-    Main,
-    Play,
-    Options,
-}
-
-impl Default for Scene {
-    fn default() -> Self {
-        Scene::Main
-    }
-}
-
-impl Scene {
-    fn fmt(&self) -> &str {
-        match self {
-            Scene::Main => "Main",
-            Scene::Play => "Play",
-            Scene::Options => "Options",
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Console {
-    input: String,
-    style: Style,
-}
-
-impl Default for Console {
-    fn default() -> Self {
-        Console {
-            input: String::new(),
-            style: Style::default(),
-        }
-    }
-}
-
-impl Console {
-    fn reset(&mut self) {
-        self.input.clear();
-        self.style = Style::default();
-    }
-}
-
-#[derive(Debug, Default)]
-pub struct App {
-    title: String,
-    scene: Scene,
-    exit: bool,
-    console: Console,
-}
-
-impl App {
-    pub fn run(&mut self, terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> io::Result<()> {
-        while !self.exit {
-            if event::poll(std::time::Duration::from_millis(100))? {
-                if let event::Event::Key(key_event) = event::read()? {
-                    self.handle_key_event(key_event);
-                }
-            }
-            self.draw(terminal)?;
-        }
-        Ok(())
-    }
-
-    fn handle_key_event(&mut self, key_event: event::KeyEvent) {
-        if key_event.kind == KeyEventKind::Press {
-            match key_event.code {
-                KeyCode::Char('q') => self.exit = true,
-                KeyCode::Enter => {
-                    // Handle the entered command here
-                    match self.console.input.as_str() {
-                        "quit" | "exit" => {
-                            self.exit = true;
-                        }
-                        "play" => {
-                            self.console.reset();
-                            self.scene = Scene::Play;
-                        }
-                        "options" | "settings" => {
-                            self.console.reset();
-                            self.scene = Scene::Options;
-                        }
-                        "main" | "home" => {
-                            self.console.reset();
-                            self.scene = Scene::Main;
-                        }
-                        _ => {
-                            self.console.style = Style::default().fg(ratatui::style::Color::Red);
-                        }
-                    }
-                }
-                KeyCode::Char(c) => {
-                    self.console.input.push(c);
-                }
-                KeyCode::Backspace => {
-                    self.console.input.pop();
-                    self.console.style = ratatui::style::Style::default();
-                }
-                _ => {}
-            }
-        }
-    }
-
-    fn draw(&mut self, terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> io::Result<()> {
-        terminal.clear()?;
-
-        let title = Paragraph::new(ratatui::style::Stylize::italic(
-            ratatui::style::Stylize::bold(self.title.clone()),
-        ))
-        .block(Block::default().borders(Borders::ALL).title("Title"))
-        .alignment(Alignment::Center);
-
-        let screen = Paragraph::new(self.scene.fmt())
-            .block(Block::default().borders(Borders::ALL).title("Scene"))
-            .alignment(Alignment::Center);
-
-        let console = Paragraph::new(self.console.input.as_str())
-            .block(Block::default().borders(Borders::ALL).title("Console"))
-            .style(self.console.style)
-            .alignment(Alignment::Left);
-
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(2)
-            .constraints(
-                [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10),
-                ]
-                .as_ref(),
-            );
-
-        terminal.draw(|f| {
-            let chunks = chunks.split(f.area());
-            f.render_widget(title, chunks[0]);
-            f.render_widget(screen, chunks[1]);
-            f.render_widget(console, chunks[2]);
-        })?;
-
-        Ok(())
-    }
+    Ok(())
 }
